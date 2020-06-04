acdebdd432dbfd57172aa78efda693f10f87a866
dolphius21 <johnrudolphlee@gmail.com>
2020-06-04 15:32:49 +0800

    hide list

--- DOM/app.js
+++ DOM/app.js
@@ -1,8 +1,20 @@
-const input = document.querySelector('input');
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const changeListDescription = document.querySelector('button.description');
 
-button. addEventListener('click', () => {
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block'; 
+  } else {
+    toggleList.textContent = 'Show list';
+    listDiv.style.display = 'none';
+  }
+});
+
+changeListDescription.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 });
 
