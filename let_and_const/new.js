7a7bcd8ffbf2d0cffc24ac1483cb1af14dddf61a
dolphius21 <johnrudolphlee@gmail.com>
2020-06-09 21:30:46 +0800

    refactoring code

--- let_and_const/new.js
+++ let_and_const/new.js
@@ -39,15 +39,13 @@ function addNewTeachers(newTeachers) {
 function printTreehouseSummary() {
 	// TODO: fix this function so that it prints the correct number of courses and   teachers 
 	
-  teachers.forEach(function(teachers) {
+  teachers.forEach((teachers) => {
     console.log(`${teachers.name} teaches ${teachers.topicArea}`);
   });
 
-  let javascript = teachers.filter(function(val) {
-    return val.topicArea === 'Javascript';
-  });
+  let javascriptTeachersCount = teachers.filter(val => val.topicArea === 'Javascript').length;
 	
-  console.log(`Treehouse has ${courses.length} JavaScript courses, and ${javascript.length} Javascript teachers`);
+  console.log(`Treehouse has ${courses.length} JavaScript courses, and ${javascriptTeachersCount} Javascript teachers`);
 }
 
 addNewTeachers(newTeachers);
