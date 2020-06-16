e60a3ec9b6613b70dd4c7e3fbe6b3433e215ce69
dolphius21 <johnrudolphlee@gmail.com>
2020-06-16 21:07:06 +0800

    refactor app.js

--- object_challenge/app.js
+++ object_challenge/app.js
@@ -1,36 +1,44 @@
-let html = '';
-let student;
-found = false;
-let studentsFound = [];
+const students = [
+  { name: 'Jeffrey', track: 'Front End Web Development', achievements: '221', points: '56,931' },
+  { name: 'Annie', track: 'Java Web Development', achievements: '219', points: '46,781' },
+  { name: 'Troy', track: 'Object-Oriented PHP', achievements: '194', points: '40,321' },
+  { name: 'Abed', track: 'Learn Ruby', achievements: '253', points: '59,531' },
+  { name: 'Britta', track: 'Data Analysis', achievements: '178', points: '38,792' },
+  { name: 'Pierce', track: 'Web Design', achievements: '187', points: '40,036' },
+  { name: 'Shirley', track: 'Rails Development', achievements: '210', points: '50,777' },
+  { name: 'Jeffrey', track: 'Web Design', achievements: '122', points: '30,536' },
+];
 
-function print(message) {
-  document.getElementById('output').innerHTML = message;
-}
+const print = (message) => {
+  const output = document.getElementById('output');
+  output.innerHTML = message;
+};
 
-function getStudentRecord(student) {
-  let record = `<h2>Student: ${student.name}</h2>`;
-  record += `<p>Track: ${student.track}</p>`;
-  record += `<p>Points: ${student.points}</p>`;
-  record += `<p>Achievements: ${student.achievements}</p>`;
-  return record;
-}
+const getStudentData = (student) => {
+  let data = `<h2>Student: ${student.name}</h2>`;
+  data += `<p>Track: ${student.track}</p>`;
+  data += `<p>Points: ${student.points}</p>`;
+  data += `<p>Achievements: ${student.achievements}</p>`;
+  return data;
+};
+
+let found = false;
+let result = '';
 
 while (true) {
-  let search = prompt(`Search student records: [John] (or type "quit" to exit.)`);
-  if ( search === null || search.toLowerCase() === 'quit' ) {
+  let search = prompt('Search student Records: type a name [Jeffrey] (or type "quit" to end and see search results)');
+  if (search === null || search.toLowerCase() === 'quit') {
     break;
   }
-  for ( let i = 0; i < students.length; i++) {
-    student = students[i];
-    if ( student.name.toLowerCase() === search.toLowerCase() ) {
-      html += getStudentRecord(student);
+  students.forEach((student) => {
+    if (student.name.toLowerCase() === search.toLowerCase()) {
+      result += getStudentData(student);
       found = true;
     }
-  }
-  if (found){
-    print(html);
+  });
+  if (found) {
+    print(result);
   } else {
-    print(`student ${search} is not found`);
+    print(`There's no student named ${search} in the records.`);
   }
 }
-
