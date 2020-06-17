0277e889598bc1c8b2b4e5be054915806fdc5c85
dolphius21 <johnrudolphlee@gmail.com>
2020-06-17 12:08:40 +0800

    refactor code

--- refactor_challenge/app.js
+++ refactor_challenge/app.js
@@ -2,12 +2,12 @@ let color = '';
 
 const randomValue = () => Math.floor(Math.random() * 256 );
 
-const randomColor = () => `rgb(${randomValue()},${randomValue()},${randomValue()})`;
+const randomColor = (value) => `rgb(${value()},${value()},${value()})`;
 
 const print = (message) =>  document.querySelector('main').innerHTML = message;
 
 for ( let i = 0; i < 10; i++ ) {
-  color += `<div style="background-color: ${randomColor()}">${i}</div>`;
+  color += `<div style="background-color: ${randomColor(randomValue)}">${i}</div>`;
 }
 
 print(color);
