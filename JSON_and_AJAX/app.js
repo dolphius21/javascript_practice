e2912a9f5c9798faaa1bcde28f86cfea2d89e4aa
dolphius21 <johnrudolphlee@gmail.com>
2020-06-30 16:58:21 +0800

    JSON and AJAX

--- JSON_and_AJAX/app.js
+++ JSON_and_AJAX/app.js
@@ -1,23 +1,46 @@
 const btn = document.getElementById('btn');
+const petInfoDiv = document.getElementById('animal-info');
+let clickCounter = 1;
 
-const buildHTML = (data) => {
-  let petInfo = document.getElementById('animal-info');
+const renderHTML = data => { 
   let html = '';
-  data.forEach(data => {
-    html += `${data.name} is a ${data.species}`;
+  data.forEach(obj => {
+    html += `<p>${obj.name} is a ${obj.species} that likes to eat`;
+    obj.foods.likes.forEach((food, index) => {
+      if (index === 0) {
+        html += ` ${food}`;
+      } else {
+        html += ` and ${food}`;
+      }
+    });
+    html += ` and dislikes`
+    obj.foods.dislikes.forEach((food, index) => {
+      if (index === 0) {
+        html += ` ${food}`;
+      } else {
+        html += ` and ${food}`;
+      }
+    });
+    html += `.</p>`
   });
+  petInfoDiv.insertAdjacentHTML('beforeend', html);
 };
 
 btn.addEventListener('click', () => {
   const petDataRequest = new XMLHttpRequest();
-  petDataRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  petDataRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${clickCounter}.json`);
 
   petDataRequest.onload = () => {
     let petData = JSON.parse(petDataRequest.responseText);
-    buildHTML(petData);
+    renderHTML(petData);
   };
 
-  catDataRequest.send();
+  petDataRequest.send();
+  clickCounter++;
+  if (clickCounter > 3) {
+    btn.classList.add('hide-me');
+  }
 }); 
+
 
 
