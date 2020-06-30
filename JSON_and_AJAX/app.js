bf3880375c8e70354ab479ed3deb3cfbe8232709
dolphius21 <johnrudolphlee@gmail.com>
2020-06-30 22:50:40 +0800

    Humans in space

--- JSON_and_AJAX/app.js
+++ JSON_and_AJAX/app.js
@@ -31,9 +31,15 @@ btn.addEventListener('click', () => {
   petDataRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${clickCounter}.json`);
 
   petDataRequest.onload = () => {
-    let petData = JSON.parse(petDataRequest.responseText);
-    renderHTML(petData);
+    if (petDataRequest.status >= 200 && < 400) {
+      let petData = JSON.parse(petDataRequest.responseText);
+      renderHTML(petData);
+    } else {
+      console.log('We connected to the server, but it returned an error.');
+    }
   };
+
+  petDataRequest.onerror = () => console.log('Connection error');
 
   petDataRequest.send();
   clickCounter++;
--- humans_in_space/app.js
+++ humans_in_space/app.js
@@ -1,16 +1,16 @@
-const astroUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary';
-const astronautsList = document.getElementById('astronauts-list');
+const astronautsDataUrl = 'http://api.open-notify.org/astros.json';
+const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+const peopleList = document.getElementById('astronauts-list');
 const btn = document.querySelector('button');
 
 // To make an AJAX request
-const getJSON = (url) => {
+const getJSON = (url, callback) => {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = () => {
     if (xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
@@ -18,7 +18,7 @@ const getJSON = (url) => {
 
 const generateHTML = (data) => {
   const section = document.createElement('section');
-  astronautsList.appendChild(section);
+  peopleList.appendChild(section);
   section.innerHTML = `
       <img src=${data.thumbnail.source}>
       <h2>${data.title}</h2>
@@ -26,3 +26,18 @@ const generateHTML = (data) => {
       <p>${data.extract}</p>
     `;
 };
+
+btn.addEventListener('click', (e) => {
+  getJSON(astronautsDataUrl, (json) => {
+    json.people.map(person => {
+      if (person.name === "Anatoly Ivanishin") {
+        person.name = "Anatoli Ivanishin";
+      }
+      if (person.name === "Chris Cassidy") {
+        person.name = "Christopher Cassidy";
+      }
+      getJSON(wikiUrl + person.name, generateHTML);
+    });
+  });
+  e.target.remove();
+});
