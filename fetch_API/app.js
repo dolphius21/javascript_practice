263c4dc4bbbbf243b04660ca2c65ad5fff831969
dolphius21 <johnrudolphlee@gmail.com>
2020-07-08 18:37:27 +0800

    fetch data

--- fetch_API/app.js
+++ fetch_API/app.js
@@ -2,28 +2,40 @@ const select = document.getElementById('breeds');
 const card = document.querySelector('.card'); 
 const form = document.querySelector('form');
 
-// ------------------------------------------
 //  FETCH FUNCTIONS
-// ------------------------------------------
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
-  .then(data => console.log(data.message))
+const fetchData = url => {
+  return fetch(url)
+          .then(response => response.json())
+};
 
+fetchData('https://dog.ceo/api/breeds/list')
+  .then(data => generateOptions(data.message))
 
-// ------------------------------------------
+fetchData('https://dog.ceo/api/breeds/image/random')
+  .then(data => generateImage(data.message))
+
 //  HELPER FUNCTIONS
-// ------------------------------------------
+const generateOptions = data => {
+  let options = '';
+  data.map(item => {
+    options += `<option value="${item}">${item}</option>`
+  });
+  select.innerHTML = options;
+};
+
+const generateImage = data => {
+  const html = `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}</p>
+  `
+  card.innerHTML = html;
+}; 
 
 
 
-// ------------------------------------------
 //  EVENT LISTENERS
-// ------------------------------------------
 
 
 
-// ------------------------------------------
 //  POST DATA
-// ------------------------------------------
 
- 
