2d027c614e4c4c5e8215ca13b59ac3c372ec9ac9
dolphius21 <johnrudolphlee@gmail.com>
2020-07-16 14:25:21 +0800

    Four in a row

---
+++ four_in_a_row/js/Player.js
@@ -0,0 +1,10 @@
+class Player {
+   constructor(name, color, token, isTurn = false) {
+      this.name = name;
+      this.id = id;
+      this.color = color;
+      this.token = token
+      this.isTurn = isTurn;
+      this.tokens = [];
+   }
+}
