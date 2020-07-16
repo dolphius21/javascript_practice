c8bda0536a652bf20a1e2e11b7d66f59ebcc8845
dolphius21 <johnrudolphlee@gmail.com>
2020-07-16 17:07:17 +0800

    create tokens

--- four_in_a_row/js/Player.js
+++ four_in_a_row/js/Player.js
@@ -3,8 +3,18 @@ class Player {
       this.name = name;
       this.id = id;
       this.color = color;
-      this.token = token
       this.isTurn = isTurn;
-      this.tokens = [];
+      this.tokens = this.createTokens(21);
+   }
+
+   createTokens(number) {
+      const tokens = [];
+
+      for (let i = 0; i < number; i++) {
+         let token = new Token(i, this);
+         tokens.push(token);
+      }
+
+      return tokens;
    }
 }
