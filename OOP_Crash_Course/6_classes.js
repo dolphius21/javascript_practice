1c74076b062831e2ec739611f319fb0d67e8c29b
dolphius21 <johnrudolphlee@gmail.com>
2020-07-17 17:59:49 +0800

    classes

---
+++ OOP_Crash_Course/6_classes.js
@@ -0,0 +1,32 @@
+class Book {
+   constructor(title, author, year) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+   }
+
+   getSummary() {
+      return `${this.title} was written by ${this.author} in ${this.year}`;
+   }
+   getAge() {
+      const years = new Date().getFullYear() - this.year;
+      return `${this.title} is ${years} years old`;
+   }
+
+   revise(newYear) {
+      this.year = newYear;
+      this.revised = true;
+   }
+
+   static topBookStore() {
+      return 'Barnes & Noble';
+   }
+}
+
+// Instantiate
+const book1 = new Book("Philosopher's Stone", "J.K Rowling", "1997");
+const book2 = new Book("The Final Empire", "Brandon Sanderson", "2006");
+
+console.log(book1);
+console.log(book2.getSummary());
+console.log(Book.topBookStore());
