ee9d8630d210b66390f9c92e6893178d3b36df73
dolphius21 <johnrudolphlee@gmail.com>
2020-07-17 15:46:50 +0800

    inheritance

--- OOP_Crash_Course/4_inheritance.js
+++ OOP_Crash_Course/4_inheritance.js
@@ -8,4 +8,22 @@ function Book(title, author, year) {
 // getSummary
 Book.prototype.getSummary = function() {
    return `${this.title} was written by ${this.author} in ${this.year}`; 
+};
+
+// Magazine Constructor
+function Magazine(title, author, year, month) {
+   Book.call(this, title, author, year);
+   this.month = month;
 }
+
+// Instantiate Prototype
+Magazine.prototype = Object.create(Book.prototype);
+
+// Instantiate
+const mag1 = new Magazine('Mag One', 'John Doe', '2018', 'Jan');
+
+// Use Magazine Constructor
+Magazine.prototype.constructor = Magazine;
+
+console.log(mag1);
+console.log(mag1.getSummary());
